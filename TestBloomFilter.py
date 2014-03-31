04f95655d8b8152e22152b32d68ed927c2305ddb
tbrown1979 <tbrown@uni.edu>
2014-03-30 20:29:09 -0500

    Adding tests, and more functionality for bloom filter.

---
+++ TestBloomFilter.py
@@ -0,0 +1,30 @@
+import random
+import unittest
+from bloom_filter import *
+
+class TestBloomFilter(unittest.TestCase):
+
+    def setUp(self):
+        self.bloomFilter = BloomFilter()
+
+    def test_vector_length(self):
+        self.assertEqual(self.bloomFilter.vectorLength, 9593)
+
+    def test_num_of_hashes(self):
+        self.assertEqual(self.bloomFilter.numOfHashes, 7)
+
+    def test_num_of_hashes_fixed_m(self):
+        self.assertEqual(self.bloomFilter._getNumHashesWithFixedM(9593, 1000), 7)
+
+    # def test
+
+class TestHashGenerator(unittest.TestCase):
+
+    def setUp(self):
+        self.generator = HashGenerator()
+
+    def test_that_returned_function_generates_value(self):
+        self.assertLess(self.generator.generateHash(1000)(), 1001), 
+
+if __name__ == '__main__':
+    unittest.main()
